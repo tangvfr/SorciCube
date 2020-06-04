2b7a7555ea98cb696601fc186ce05275260d4890
tangv <tangv@DenisComputer>
2020-06-04 16:22:39 +0200

    add new cart comparator

--- src/main/java/fr/tangv/sorcicubespell/carts/CartComparator.java
+++ src/main/java/fr/tangv/sorcicubespell/carts/CartComparator.java
@@ -9,6 +9,48 @@ public enum CartComparator implements Comparator<Cart> {
 		public int compare(Cart c1, Cart c2) {
 			return c1.getId().compareTo(c2.getId());
 		}
+	}),
+	BY_FACTION(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c1.getFaction().ordinal(), c2.getFaction().ordinal());
+		}
+	}),
+	BY_RARITY(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c1.getRarity().ordinal(), c2.getRarity().ordinal());
+		}
+	}),
+	BY_TYPE(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c1.getType().ordinal(), c2.getType().ordinal());
+		}
+	}),
+	BY_LOW_DAMAGE(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c1.getDamage(), c2.getDamage());
+		}
+	}),
+	BY_HIGH_DAMAGE(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c2.getDamage(), c1.getDamage());
+		}
+	}),
+	BY_LOW_MANA(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c1.getCountMana(), c2.getCountMana());
+		}
+	}),
+	BY_HIGH_MANA(new Comparator<Cart>() {
+		@Override
+		public int compare(Cart c1, Cart c2) {
+			return Integer.compare(c2.getCountMana(), c1.getCountMana());
+		}
 	});
 	
 	private Comparator<Cart> comparatorCart;
