89dd063c78b2a5174cccbb69e3d9d27392c611ca
tangv <tangv@DenisComputer>
2020-06-24 17:32:14 +0200

    Add PlayerCards is forget it last commits

---
+++ src/main/java/fr/tangv/sorcicubespell/player/PlayerCards.java
@@ -0,0 +1,12 @@
+package fr.tangv.sorcicubespell.player;
+
+public class PlayerFeature {
+
+	private List<DeckPlayer> decks;
+	private int numberDeck;
+	
+	public PlayerFeature(Player player) {
+		
+	}
+	
+}
