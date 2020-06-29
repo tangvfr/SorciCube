aed59bfb5f2a077ac4135c51130a182b733d9707
tangv <tangv@DenisComputer>
2020-06-29 21:42:33 +0200

    improve render card

--- src/main/java/fr/tangv/sorcicubespell/card/CardRender.java
+++ src/main/java/fr/tangv/sorcicubespell/card/CardRender.java
@@ -10,6 +10,10 @@ import fr.tangv.sorcicubespell.util.ItemBuild;
 
 public class CardRender {
 
+	private static String featureToString(SorciCubeSpell sorci, CardFeature feature) {
+		return feature.getValue().toString();
+	}
+	
 	public static ItemStack cardToItem(Card card, SorciCubeSpell sorci) {
 		return CardRender.cardToItem(card, sorci, 1, false);
 	}
@@ -20,34 +24,31 @@ public class CardRender {
 		CardFeatures features = card.getFeatures();
 		String cible = sorci.getEnumTool().cibleToString(card.getCible())+
 				(card.getCibleFaction() != CardFaction.BASIC ? (" "+sorci.getEnumTool().factionToString(card.getCibleFaction())) : "");
-		String name = card.getName()+" §8/ §b2\u2756";
+		String name = card.getName()+"§r §b2\u2756";
 		ArrayList<String> lore = new ArrayList<String>();
 		//rarity and faction
-		lore.add(sorci.getEnumTool().factionToString(card.getFaction())
-				+" §f| "
-				+sorci.getEnumTool().rarityToString(card.getRarity()));
-		lore.add("");
+		lore.add("  "+sorci.getEnumTool().factionToString(card.getFaction()));
+		lore.add("  "+sorci.getEnumTool().rarityToString(card.getRarity()));
 		//damage and health if is entity
 		if (card.getType() == CardType.ENTITY) {
-			lore.add("      §e"+features.getFeature(CardFeatures.ATTACK_DAMMAGE).getValue().toString()+"\u2694"
-					+" §c"+features.getFeature(CardFeatures.HEALTH).getValue().toString()+"\u2665");
-			lore.add("");
+			lore.add("    §e"+features.getFeature(CardFeatures.ATTACK_DAMMAGE).getValue().toString()+" \u2694"
+					+" §c"+features.getFeature(CardFeatures.HEALTH).getValue().toString()+" \u2665");
 		}
+		lore.add("");
 		//features
 		for (CardFeature feature : features.listFeatures())
-			lore.add(sorci.getEnumTool().featureToString(feature.getType())
-					.replace("{"+feature.getValue().getType().name().toLowerCase()+"}", feature.getValue().toString())
-					.replace("{cible}", cible)
-				);
+			if (feature.getType() != CardFeatureType.SKIN && feature.getType() != CardFeatureType.HEALTH)
+				lore.add(sorci.getEnumTool().featureToString(feature.getType())
+						.replace("{"+feature.getValue().getType().name().toLowerCase()+"}", featureToString(sorci, feature))
+						.replace("{cible}", cible)
+					);
 		//lore
 		lore.add("");
 		for (int i = 0; i < card.getDescription().size(); i++)
 			lore.add(card.getDescription().get(i));
 		//type
 		lore.add("");
-		lore.add("");
 		lore.add(sorci.getEnumTool().typeToString(card.getType()));
-		lore.add("");
 		lore.add("");
 		//id
 		lore.add("§8Id: "+card.getUUID());
