6c942ae17e31b4cf6e4aa1dcd4028b46f88392e1
tangvfr <tangv@DenisComputer>
2020-07-16 18:45:37 +0200

    replace itemhead by new texture

--- src/main/java/fr/tangv/sorcicubespell/util/ItemHead.java
+++ src/main/java/fr/tangv/sorcicubespell/util/ItemHead.java
@@ -8,12 +8,12 @@ public class ItemHead {
 		return ItemBuild.buildSkull(url, 1, "", null, false);
 	}
 	
-	public final static ItemStack SELECTABLE_SPELL = createHead("http://textures.minecraft.net/texture/7db9629cab72e021c8b561650c586b99bfdb3f6a9a962cc6e5611222d3558ef");
-	public final static ItemStack SELECTABLE_ATTACK = createHead("http://textures.minecraft.net/texture/3e4f2f9698c3f186fe44cc63d2f3c4f9a241223acf0581775d9cecd7075");
-	public final static ItemStack SELECTABLE_DAMAGE = createHead("http://textures.minecraft.net/texture/741941e7e7e914a115c342d6d38a22931e138b3da1eeb4e998571e90f871517");
-	public final static ItemStack SELECTABLE_DEAD = createHead("http://textures.minecraft.net/texture/a3852bf616f31ed67c37de4b0baa2c5f8d8fca82e72dbcafcba66956a81c4");
-	public final static ItemStack SELECTABLE_SPAWN = createHead("http://textures.minecraft.net/texture/d8aab6d9a0bdb07c135c97862e4edf3631943851efc545463d68e793ab45a3d3");
-	public final static ItemStack SELECTABLE_POSE = createHead("http://textures.minecraft.net/texture/53c1a4cf49514b57a78c15d484fc0eaa87f2fc6487cd54ac3b5807f64c6a72d");
-	public final static ItemStack SELECTED_ENTITY = createHead("http://textures.minecraft.net/texture/b221da4418bd3bfb42eb64d2ab429c61decb8f4bf7d4cfb77a162be3dcb0b927");
+	public final static ItemStack SELECTABLE_SPELL = createHead("http://textures.minecraft.net/texture/d143855134e3a477b0e83c1a8a93b157b68efd662e9bb6d490520c08d5392446");
+	public final static ItemStack SELECTABLE_ATTACK = createHead("http://textures.minecraft.net/texture/ed75e9ed3763518c048ad3e035e284f5dbf04a12ea9fa0c241214d362ad7ed76");
+	public final static ItemStack SELECTABLE_DAMAGE = createHead("http://textures.minecraft.net/texture/2b1d13a0b3f3687d05e2bd0ae7c0aebd8ae00a8e924de62b84dc15fe0096c6b3");
+	public final static ItemStack SELECTABLE_DEAD = createHead("http://textures.minecraft.net/texture/5d1c700e085819930eaacd4785310d0347a705f3ebc3daa73c0edc5db3145940");
+	public final static ItemStack SELECTABLE_SPAWN = createHead("http://textures.minecraft.net/texture/e271151a0f46ced9b811a6426ae868b033470281266a1b41e690f2c8bd2b3cb0");
+	public final static ItemStack SELECTABLE_POSE = createHead("http://textures.minecraft.net/texture/8d4385db7adce92077c81943852a8e51e678d199eb899ed3a065c408d586d215");
+	public final static ItemStack SELECTED_ENTITY = createHead("http://textures.minecraft.net/texture/8f9b1e4f03ae372b6c95a0e5d1e3d9b59f5a7db5d39f7b58d2303ad217059a0e");
 	
 }
