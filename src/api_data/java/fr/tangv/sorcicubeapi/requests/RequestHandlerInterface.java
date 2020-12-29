585d7ffae4746a680dac13527fa7ab0bd0223cdf
tangvfr <tangv.minecraft@gmail.com>
2020-12-29 12:15:31 +0100

    path last

--- src/api_data/java/fr/tangv/sorcicubeapi/requests/RequestHandlerInterface.java
+++
@@ -1,9 +0,0 @@
-package fr.tangv.sorcicubeapi.requests;
-
-import fr.tangv.sorcicubeapi.clients.Client;
-
-public interface RequestHandlerInterface {
-
-	public void handlingRequest(Client client, Request request) throws Exception;
-	
-}
