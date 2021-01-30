c43d472efd13be8745eb513032c603f86ac3caf7
tangvfr <tangv.minecraft@gmail.com>
2021-01-30 15:06:45 +0100

    path handler reponse

--- SorciCubeCore/src/java/fr/tangv/sorcicubecore/sorciclient/HandlerReponse.java
+++ SorciCubeCore/src/java/fr/tangv/sorcicubecore/sorciclient/HandlerReponse.java
@@ -1,6 +1,7 @@
 package fr.tangv.sorcicubecore.sorciclient;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 import fr.tangv.sorcicubecore.clients.Client;
 import fr.tangv.sorcicubecore.requests.Request;
@@ -8,50 +9,54 @@ import fr.tangv.sorcicubecore.requests.RequestHandlerInterface;
 
 public class HandlerReponse implements RequestHandlerInterface {
 
-	//public final ConcurrentHashMap<Integer, Request> reponsesWait;
+	public final ConcurrentHashMap<Integer, ReponseRequest> reponsesWait;
 	public final SorciClient sorci;
 	public final long timeout;
-	private volatile Request request;
-	private volatile Request reponse;
 	
 	public HandlerReponse(SorciClient sorci, long timeout) {
-		//this.reponsesWait = new ConcurrentHashMap<Integer, Request>();
+		this.reponsesWait = new ConcurrentHashMap<Integer, ReponseRequest>();
 		this.sorci = sorci;
 		this.timeout = timeout;
 	}
 
 	@Override
-	public synchronized void handlingRequest(Client client, Request reponse) throws Exception {
-		//System.out.println("RID: "+reponse.id);
-		/*
-		Request request = reponsesWait.get(reponse.id);
-		if (request != null) {
-			reponsesWait.replace(reponse.id, reponse);
-			request.name.notifyAll();
-		}*/
-		if (request != null) {
-			System.out.println("RID: "+reponse.id);
-			
-			if (reponse.id == request.id) {
-				this.reponse = reponse;
-				this.notifyAll();
-			}
-		}
+	public void handlingRequest(Client client, Request reponse) throws Exception {
+		ReponseRequest rep = reponsesWait.remove(reponse.id);
+		if (rep != null)
+			rep.reponseRequest(reponse);
 	}
 	
-	public synchronized Request sendRequestReponse(Request request) throws IOException {
-		this.reponse = null;
-		this.request = request;
-		//reponsesWait.put(request.id, request);
-		sorci.sendRequest(request);
-		try {
-			this.wait(timeout);
-		} catch (InterruptedException e) {
-			return null;
+	public Request sendRequestReponse(Request request) throws IOException {
+		ReponseRequest rep =  new ReponseRequest(request);
+		reponsesWait.put(request.id, rep);
+		return rep.sendRequestReponse();
+	}
+	
+	private class ReponseRequest {
+		
+		private final Request request;
+		private volatile Request reponse;
+		
+		public ReponseRequest(Request request) {
+			this.request = request;
+			this.reponse = null;
 		}
-		System.out.println("R<RID: "+request.id);
-		this.request = null;
-		return reponse;
+		
+		public synchronized void reponseRequest(Request reponse) throws Exception {
+			this.reponse = reponse;
+			this.notifyAll();
+		}
+		
+		public synchronized Request sendRequestReponse() throws IOException {
+			sorci.sendRequest(request);
+			try {
+				this.wait(timeout);
+			} catch (InterruptedException e) {
+				return null;
+			}
+			return reponse;
+		}
+		
 	}
 	
 }
