6c340548d41ef8850be82edf99a99256eba96579
tangvfr <tangv.minecraft@gmail.com>
2021-01-31 14:46:26 +0100

    chnage design others panel

---
+++ SorciCubeApp/src/java/fr/tangv/sorcicubeapp/others/PanelOthers.java
@@ -0,0 +1,72 @@
+package fr.tangv.sorcicubeapp.others;
+
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Desktop.Action;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import fr.tangv.sorcicubeapp.connection.FrameLogi;
+import fr.tangv.sorcicubeapp.tools.ImageTool;
+import fr.tangv.sorcicubeapp.utils.ClickListener;
+import fr.tangv.sorcicubecore.sorciclient.SorciClient;
+
+public class PanelOthers extends JPanel {
+
+	private static final long serialVersionUID = -1979640189589135131L;
+
+	public PanelOthers(SorciClient client, FrameLogi logi) {
+		this.setLayout(new GridLayout(3, 1, 10, 10));
+		int border = 20;
+		this.setBorder(new EmptyBorder(border, border, border, border));
+		
+		//mineSkin
+		JButton mineskin = new JButton("mineskin.org");
+		mineskin.addMouseListener(new ClickListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Action.BROWSE))
+					try {
+						Desktop.getDesktop().browse(new URI("https://mineskin.org/"));
+					} catch (IOException | URISyntaxException e1) {
+						e1.printStackTrace();
+					}
+			}
+		});
+		this.add(mineskin);
+		
+		//imageTool
+		JButton imageTool = new JButton("Image Tool");
+		imageTool.addMouseListener(new ClickListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(PanelOthers.this, new ImageTool(), "Image Tool", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		this.add(imageTool);
+		
+		//disconnect
+		JButton disconnect = new JButton("Disconnect");
+		disconnect.addMouseListener(new ClickListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					client.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					logi.showConnection("DisconnectError: "+e1.getMessage(), Color.RED);
+				}
+			}
+		});
+		this.add(disconnect);
+	}
+	
+}
--- SorciCubeApp/src/java/fr/tangv/sorcicubeapp/tabs/TabsPanel.java
+++ SorciCubeApp/src/java/fr/tangv/sorcicubeapp/tabs/TabsPanel.java
@@ -1,24 +1,12 @@
 package fr.tangv.sorcicubeapp.tabs;
 
-import java.awt.Color;
-import java.awt.Desktop;
-import java.awt.Desktop.Action;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import fr.tangv.sorcicubeapp.card.CardsPanel;
 import fr.tangv.sorcicubeapp.card.PanelFilter.PanelFilterException;
 import fr.tangv.sorcicubeapp.connection.FrameLogi;
-import fr.tangv.sorcicubeapp.tools.ImageTool;
-import fr.tangv.sorcicubeapp.utils.ClickListener;
+import fr.tangv.sorcicubeapp.others.PanelOthers;
 import fr.tangv.sorcicubecore.requests.RequestException;
 import fr.tangv.sorcicubecore.sorciclient.ReponseRequestException;
 import fr.tangv.sorcicubecore.sorciclient.SorciClient;
@@ -27,56 +15,16 @@ public class TabsPanel extends JTabbedPane {
 
 	private static final long serialVersionUID = -4754481175431522229L;
 
-	private final SorciClient client;
-	private final FrameLogi logi;
 	//tabbed
 	private final CardsPanel cardsPanel;
-	private final JPanel others;
+	private final PanelOthers others;
 	
 	public TabsPanel(SorciClient client, FrameLogi logi) throws IOException, ReponseRequestException, RequestException, PanelFilterException {
-		this.client = client;
-		this.logi = logi;
 		this.setTabPlacement(JTabbedPane.TOP);
-		
 		//init tabbed
 		this.cardsPanel = new CardsPanel(client, logi);
-		//other
-		this.others = new JPanel();
-		this.others.setLayout(new BoxLayout(others, BoxLayout.Y_AXIS));
-		JButton mineskin = new JButton("mineskin.org");
-		mineskin.addMouseListener(new ClickListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Action.BROWSE))
-					try {
-						Desktop.getDesktop().browse(new URI("https://mineskin.org/"));
-					} catch (IOException | URISyntaxException e1) {
-						e1.printStackTrace();
-					}
-			}
-		});
-		others.add(mineskin);
-		JButton imageTool = new JButton("Image Tool");
-		imageTool.addMouseListener(new ClickListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(others, new ImageTool(), "Image Tool", JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-		others.add(imageTool);
-		JButton disconnect = new JButton("Disconnect");
-		disconnect.addMouseListener(new ClickListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				try {
-					client.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-					cardsPanel.getFrameLogi().showConnection("DisconnectError: "+e1.getMessage(), Color.RED);
-				}
-			}
-		});
-		others.add(disconnect);
+		this.others = new PanelOthers(client, logi);
+		
 		//add tabbbed
 		this.addTab("Cards", this.cardsPanel);
 		this.addTab("Others", this.others);
