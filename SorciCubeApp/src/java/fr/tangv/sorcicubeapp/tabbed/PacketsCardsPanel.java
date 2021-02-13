3f4503d33bdf6966a9d7cd13727db10245f19fc8
tangvfr <tangv.minecraft@gmail.com>
2021-02-13 14:55:25 +0100

    add searchpanel

--- SorciCubeApp/src/java/fr/tangv/sorcicubeapp/tabbed/PacketsCardsPanel.java
+++ SorciCubeApp/src/java/fr/tangv/sorcicubeapp/tabbed/PacketsCardsPanel.java
@@ -1,32 +1,22 @@
 package fr.tangv.sorcicubeapp.tabbed;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Vector;
 
-import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
-import javax.swing.JSplitPane;
-import javax.swing.JTextField;
-import javax.swing.ListCellRenderer;
-import javax.swing.ListSelectionModel;
-import javax.swing.border.EmptyBorder;
 
 import fr.tangv.sorcicubeapp.connection.FrameLogi;
-import fr.tangv.sorcicubeapp.utils.ClickListener;
 import fr.tangv.sorcicubeapp.utils.ColorMCToHTML;
 import fr.tangv.sorcicubecore.handler.HandlerPacketCards;
 import fr.tangv.sorcicubecore.packet.PacketCards;
@@ -34,95 +24,21 @@ import fr.tangv.sorcicubecore.requests.RequestException;
 import fr.tangv.sorcicubecore.sorciclient.ReponseRequestException;
 import fr.tangv.sorcicubecore.sorciclient.SorciClient;
 
-public class PacketsCardsPanel extends JSplitPane {
+public class PacketsCardsPanel extends SearchPanel<PacketCards> {
 
 	private static final long serialVersionUID = 2460248920367644846L;
 	
-	private final FrameLogi logi;
-	private final HandlerPacketCards handler;
-	private final JButton refresh;
-	private final JLabel clear;
-	private final JTextField search;
-	private final JList<PacketCards> listPakcets;
-	private final PacketCardsPanel packetCard;
+	private HandlerPacketCards handler;
+	private PacketCardsPanel packetCard;
 	
 	public PacketsCardsPanel(SorciClient client, FrameLogi logi) throws IOException, ReponseRequestException, RequestException {
-		super(HORIZONTAL_SPLIT);
-		this.logi = logi;
+		super(client, logi);
+	}
+	
+	@Override
+	protected JComponent initSelectPanel(SorciClient client, JPopupMenu menu) throws IOException, ReponseRequestException, RequestException {
 		this.handler = new HandlerPacketCards(client);
-		//refresh
-		this.refresh = new JButton("");
-		this.refresh.addMouseListener(new ClickListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				refresh();
-			}
-		});
-		//clear
-		this.clear = new JLabel(" X ");
-		clear.addMouseListener(new ClickListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				search.setText("");
-				refresh();
-			}
-		});
-		clear.setForeground(Color.RED);
-		//info
-		this.search = new JTextField();
-		search.setToolTipText("name of packet");
-		search.addKeyListener(new KeyListener() {
-			@Override
-			public void keyTyped(KeyEvent e) {}
-			@Override
-			public void keyReleased(KeyEvent e) {}
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					refresh();
-				}
-			}
-		});
-		//main panel
-		this.listPakcets = new JList<PacketCards>();
-		this.listPakcets.setCellRenderer(new ListCellRenderer<PacketCards>() {
-			private String antiSpaces(String input) {
-				if (input.startsWith(" "))
-					return input.replaceFirst(" ", "§4[*space*]§0");
-				else
-					return input;
-			}
-			@Override
-			public Component getListCellRendererComponent(JList<? extends PacketCards> list, PacketCards value, int index, boolean isSelected, boolean cellHasFocus) {
-				return new JLabel(
-						"<html><body><span>"
-						+(isSelected ? ">" : "")+"</span>"
-						+ColorMCToHTML.replaceColor(antiSpaces(value.getName()))
-						+"</html></body>"
-					);
-			}
-		});
-		this.listPakcets.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.listPakcets.addMouseListener(new ClickListener() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				packetCard.showPacket(listPakcets.getSelectedValue());
-				packetCard.repaint();
-			}
-		});
 		this.packetCard = new PacketCardsPanel(this);
-		//makeup
-		JPanel panelUp = new JPanel(new BorderLayout());
-		panelUp.add(refresh, BorderLayout.NORTH);
-		panelUp.add(clear, BorderLayout.WEST);
-		panelUp.add(search, BorderLayout.CENTER);
-		panelUp.setBorder(new EmptyBorder(0, 0, 5, 0));
-		JPanel panelLeft = new JPanel(new BorderLayout());
-		panelLeft.add(panelUp, BorderLayout.NORTH);
-		panelLeft.add(listPakcets, BorderLayout.CENTER);
-		this.add(panelLeft, 0);
-		this.add(this.packetCard, 1);
-		refresh();
 		//popmenu
 		JMenuItem create = new JMenuItem("Create Pakcet");
 		create.addActionListener(new ActionListener() {
@@ -145,7 +61,7 @@ public class PacketsCardsPanel extends JSplitPane {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (e.getID() == ActionEvent.ACTION_FIRST) {
-					PacketCards packet = listPakcets.getSelectedValue();
+					PacketCards packet = list.getSelectedValue();
 					if (packet != null) {
 						if (0 == JOptionPane.showConfirmDialog(logi, "Are you sure remove pakcet nommed \""+packet.getName()+"\" ?", "Remove Packet", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE)) {
 							try {
@@ -161,12 +77,13 @@ public class PacketsCardsPanel extends JSplitPane {
 				}
 			}
 		});
-		JPopupMenu menu = new JPopupMenu();
+		//menu add
 		menu.add(create);
 		menu.add(delete);
-		this.listPakcets.setComponentPopupMenu(menu);
+		return this.packetCard;
 	}
-	
+			
+	@Override
 	public void refresh() {
 		try {
 			String nameSelect = this.packetCard.getPacketName();
@@ -185,9 +102,9 @@ public class PacketsCardsPanel extends JSplitPane {
 					list.add(packet);
 				max++;
 			}
-			listPakcets.setListData(list);
+			this.list.setListData(list);
 			if (pack != null)
-				listPakcets.setSelectedValue(pack, true);
+				this.list.setSelectedValue(pack, true);
 			packetCard.showPacket(pack);
 			refresh.setText("Refresh | "+max+" packets "+list.size()+" find");
 			this.repaint();
@@ -204,6 +121,29 @@ public class PacketsCardsPanel extends JSplitPane {
 			JOptionPane.showMessageDialog(this, "Error: "+e.getMessage(), "Error update", JOptionPane.ERROR_MESSAGE);
 			logi.showConnection("Error: "+e.getMessage(), Color.PINK);
 		}
+	}
+
+	private String antiSpaces(String input) {
+		if (input.startsWith(" "))
+			return input.replaceFirst(" ", "§4[*space*]§0");
+		else
+			return input;
+	}
+	
+	@Override
+	public Component getListCellRendererComponent(JList<? extends PacketCards> list, PacketCards value, int index, boolean isSelected, boolean cellHasFocus) {
+		return new JLabel(
+				"<html><body><span>"
+				+(isSelected ? ">" : "")+"</span>"
+				+ColorMCToHTML.replaceColor(antiSpaces(value.getName()))
+				+"</html></body>"
+			);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		packetCard.showPacket(list.getSelectedValue());
+		packetCard.repaint();
 	}
 	
 }
---
+++ SorciCubeApp/src/java/fr/tangv/sorcicubeapp/tabbed/SearchPanel.java
@@ -0,0 +1,109 @@
+package fr.tangv.sorcicubeapp.tabbed;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EmptyBorder;
+
+import fr.tangv.sorcicubeapp.connection.FrameLogi;
+import fr.tangv.sorcicubeapp.utils.ClickListener;
+import fr.tangv.sorcicubecore.requests.RequestException;
+import fr.tangv.sorcicubecore.sorciclient.ReponseRequestException;
+import fr.tangv.sorcicubecore.sorciclient.SorciClient;
+
+@SuppressWarnings("serial")
+public abstract class SearchPanel<T> extends JSplitPane implements ListCellRenderer<T>, MouseListener {
+		
+	protected final FrameLogi logi;
+	protected final JButton refresh;
+	protected final JLabel clear;
+	protected final JTextField search;
+	protected final JList<T> list;
+	
+	public SearchPanel(SorciClient client, FrameLogi logi) throws IOException, ReponseRequestException, RequestException {
+		super(HORIZONTAL_SPLIT);
+		this.logi = logi;
+		//refresh
+		this.refresh = new JButton("");
+		this.refresh.addMouseListener(new ClickListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				refresh();
+			}
+		});
+		//clear
+		this.clear = new JLabel(" X ");
+		clear.addMouseListener(new ClickListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				search.setText("");
+				refresh();
+			}
+		});
+		clear.setForeground(Color.RED);
+		//info
+		this.search = new JTextField();
+		search.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {}
+			@Override
+			public void keyReleased(KeyEvent e) {}
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					refresh();
+				}
+			}
+		});
+		//main panel
+		this.list = new JList<T>();
+		this.list.setCellRenderer(this);
+		this.list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.list.addMouseListener(this);
+		//makeup
+		JPanel panelUp = new JPanel(new BorderLayout());
+		panelUp.add(refresh, BorderLayout.NORTH);
+		panelUp.add(clear, BorderLayout.WEST);
+		panelUp.add(search, BorderLayout.CENTER);
+		panelUp.setBorder(new EmptyBorder(0, 0, 5, 0));
+		JPanel panelLeft = new JPanel(new BorderLayout());
+		panelLeft.add(panelUp, BorderLayout.NORTH);
+		panelLeft.add(list, BorderLayout.CENTER);
+		this.add(panelLeft, 0);
+		JPopupMenu menu = new JPopupMenu();
+		this.add(this.initSelectPanel(client, menu), 1);
+		this.list.setComponentPopupMenu(menu);
+		refresh();
+	}
+	
+	protected abstract JComponent initSelectPanel(SorciClient client, JPopupMenu menu) throws IOException, ReponseRequestException, RequestException;
+	public abstract void refresh();
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {}
+	
+}
