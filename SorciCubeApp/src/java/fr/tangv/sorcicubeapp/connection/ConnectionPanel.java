bd19d71cfcb20ecf277f0ad53f49fc2f8c6f297b
tangvfr <tangv.minecraft@gmail.com>
2021-03-03 12:04:18 +0100

    add password save

--- SorciCubeApp/src/java/fr/tangv/sorcicubeapp/connection/ConnectionPanel.java
+++ SorciCubeApp/src/java/fr/tangv/sorcicubeapp/connection/ConnectionPanel.java
@@ -7,13 +7,25 @@ import java.awt.Label;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URISyntaxException;
-import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
@@ -24,11 +36,14 @@ import fr.tangv.sorcicubecore.sorciclient.SorciClientURI;
 public class ConnectionPanel extends JPanel {
 
 	private static final long serialVersionUID = 8194724955087227091L;
-	private JButton btnConnection;
-	private JButton btnCreate;
-	private JTextField scURI;
-	private JLabel message;
-	private JPanel centerPan;
+	private final JButton btnConnection;
+	private final JButton btnCreate;
+	private final JTextField scURI;
+	private final JMenuItem clearURI;
+	private final JLabel message;
+	private final JPanel centerPan;
+	private final Vector<String> uris;
+	private final File file;
 	
 	public ConnectionPanel(FrameLogi fl, String defaultURI) {
 		//btnconnection
@@ -38,10 +53,15 @@ public class ConnectionPanel extends JPanel {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
-					SorciClientURI uri = new SorciClientURI(scURI.getText());
+					String text = scURI.getText();
+					if (!uris.contains(text)) {
+						uris.add(text);
+						saveURIS();
+					}
+					SorciClientURI uri = new SorciClientURI(text);
 					message.setText(" ");
 					fl.tryConnection(uri);
-				} catch (NumberFormatException | UnknownHostException | URISyntaxException e1) {
+				} catch (NumberFormatException | URISyntaxException | IOException e1) {
 					message.setText("Error: "+e1.getMessage());
 					message.setForeground(Color.MAGENTA);
 				}
@@ -77,11 +97,41 @@ public class ConnectionPanel extends JPanel {
 				fl.setEnabled(false);
 			}
 		});
-		//scURI
-		scURI = new JTextField(defaultURI);
 		//message
 		message = new JLabel(" ");
 		message.setHorizontalAlignment(JLabel.CENTER);
+		//files
+		this.file = new File(System.getenv("appdata")+"/SorciCubeApp/uris");
+		this.uris = new Vector<String>();
+		try {
+			if (!file.exists()) {
+				if (!file.getParentFile().exists())
+					file.getParentFile().mkdirs();
+				file.createNewFile();
+			} else
+				loadURIS();
+		} catch (IOException e) {
+			message.setText("Error: "+e.getMessage());
+			message.setForeground(Color.MAGENTA);
+		}
+		//scURI
+		if (defaultURI.isEmpty() && !uris.isEmpty())
+			defaultURI = uris.lastElement();
+		scURI = new JTextField(defaultURI);
+		//clearURI
+		this.clearURI = new JMenuItem("Clear");
+		clearURI.addMouseListener(new ClickListener() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				uris.clear();
+				try {
+					saveURIS();
+				} catch (IOException e) {
+					message.setText("Error: "+e.getMessage());
+					message.setForeground(Color.MAGENTA);
+				}
+			}
+		});
 		//centerPan
 		centerPan = new JPanel();
 		centerPan.setLayout(new GridLayout(4, 1, 0, 5));
@@ -105,6 +155,47 @@ public class ConnectionPanel extends JPanel {
 	public void setMessage(String message, Color color) {
 		this.message.setText(message);
 		this.message.setForeground(color);
+	}
+	
+	private void updatePopMenu() {
+		JPopupMenu pop = new JPopupMenu();
+		for (String uri : uris) {
+			JMenuItem item = new JMenuItem(uri);
+			item.addMouseListener(new ClickListener() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					scURI.setText(uri);
+					scURI.repaint();
+				}
+			});
+		}
+		pop.addSeparator();
+		pop.add(clearURI);
+		scURI.setComponentPopupMenu(pop);
+	}
+	
+	private void saveURIS() throws IOException {
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8));
+		Iterator<String> it = uris.iterator();
+		while (true) {
+			out.append(it.next());
+			if (it.hasNext())
+				out.newLine();
+			else
+				break;
+		}
+		out.close();
+		updatePopMenu();
+	}
+	
+	private void loadURIS() throws IOException {
+		uris.clear();
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+		String uri;
+		while ((uri = in.readLine()) != null)
+			uris.add(uri);
+		in.close();
+		updatePopMenu();
 	}
 	
 }
