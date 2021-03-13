43cb843727cfee69f836e6635bdd23b6574ba983
tangvfr <tangv.minecraft@gmail.com>
2021-03-13 23:24:51 +0100

    PAth default deck

--- SorciCubeSpell/src/java/fr/tangv/sorcicubespell/gui/GuiSelectDefaultDeck.java
+++ SorciCubeSpell/src/java/fr/tangv/sorcicubespell/gui/GuiSelectDefaultDeck.java
@@ -84,6 +84,7 @@ public class GuiSelectDefaultDeck extends AbstractGui {
 			e1.printStackTrace();
 		}
 		if (pf != null) {
+			getPlayerGui(player).setPlayerFeature(pf);
 			player.sendMessage(sorci.getMessage().getString("message_select_default_deck")
 					.replace("{faction}", sorci.getEnumTool().factionToString(faction)));
 		} else
