e4982300acef322b217c20a12692178b3adf60a5
BuildTools <unconfigured@null.spigotmc.org>
2021-04-16 16:40:09 +0200

    remake enumtool

--- SorciCubeSpell/src/java/fr/tangv/sorcicubespell/util/EnumTool.java
+++ SorciCubeSpell/src/java/fr/tangv/sorcicubespell/util/EnumTool.java
@@ -1,7 +1,5 @@
 package fr.tangv.sorcicubespell.util;
 
-import org.bukkit.configuration.ConfigurationSection;
-
 import fr.tangv.sorcicubecore.card.CardCible;
 import fr.tangv.sorcicubecore.card.CardComparator;
 import fr.tangv.sorcicubecore.card.CardFaction;
@@ -10,6 +8,11 @@ import fr.tangv.sorcicubecore.card.CardRarity;
 import fr.tangv.sorcicubecore.card.CardType;
 import fr.tangv.sorcicubecore.card.CardValue;
 import fr.tangv.sorcicubecore.configs.CibleEnumConfig;
+import fr.tangv.sorcicubecore.configs.FactionEnumConfig;
+import fr.tangv.sorcicubecore.configs.FeatureEnumConfig;
+import fr.tangv.sorcicubecore.configs.RarityEnumConfig;
+import fr.tangv.sorcicubecore.configs.SortEnumConfig;
+import fr.tangv.sorcicubecore.configs.TypeEnumConfig;
 import fr.tangv.sorcicubespell.SorciCubeSpell;
 
 public class EnumTool {
@@ -76,43 +79,197 @@ public class EnumTool {
 				
 			case ONE_HERO:
 				return c.oneHero.value;
+				
+			default:
+				return null;
 		}
-		return null;
 	}
 	
 	public String factionToString(CardFaction faction) {
-		return faction.getColor()+this.enumToString(faction, this.faction);
+		FactionEnumConfig f = sorci.config().enums.faction;
+		switch (faction) {
+			case BASIC:
+				return f.basic.value;
+				
+			case DARK:
+				return f.dark.value;
+				
+			case LIGHT:
+				return f.light.value;
+				
+			case NATURE:
+				return f.nature.value;
+				
+			case TOXIC:
+				return f.toxic.value;
+				
+			default:
+				return null;
+		}
 	}
 	
 	public String rarityToString(CardRarity rarity) {
-		return rarity.getColor()+this.enumToString(rarity, this.rarity);
+		RarityEnumConfig r = sorci.config().enums.rarity;
+		switch (rarity) {
+			case COMMUN:
+				return r.commun.value;
+				
+			case EPIC:
+				return r.epic.value;
+						
+			case LEGENDARY:
+				return r.legendary.value;
+						
+			case RARE:
+				return r.rare.value;
+						
+			default:
+				return null;
+		}
 	}
 	
 	public String typeToString(CardType type) {
-		return this.enumToString(type, this.type);
+		TypeEnumConfig t = sorci.config().enums.type;
+		switch (type) {
+			case ENTITY:
+				 return t.entity.value;
+				
+			case SPELL:
+				return t.spell.value;
+				
+			default:
+				return null;
+		}
 	}
 	
 	public String sortToString(CardComparator sort) {
-		return this.enumToString(sort, this.sort);
+		SortEnumConfig s = sorci.config().enums.sort;
+		switch (sort) {
+			case BY_FACTION:
+				return s.byFaction.value;
+				
+			case BY_HIGH_MANA:
+				return s.byHighMana.value;
+				
+			case BY_ID:
+				return s.byId.value;
+				
+			case BY_LOW_MANA:
+				return s.byLowMana.value;
+				
+			case BY_NAME:
+				return s.byName.value;
+				
+			case BY_RARITY:
+				return s.byRarity.value;
+				
+			case BY_TYPE:
+				return s.byType.value;
+				
+			default:
+				return null;
+		}
 	}
 	
 	public String featureToString(CardFeatureType featureType, CardValue value) {
-		if ((featureType == CardFeatureType.BOOST_DAMAGE
-				|| featureType == CardFeatureType.BOOST_HEALTH
-				|| featureType == CardFeatureType.BOOST_MANA
-				|| featureType == CardFeatureType.REMOVE_MANA_HERO)
-			&& value.asNumber() < 0)
-			return this.enumToStringNeg(featureType, feature);
-		else
-			return this.enumToString(featureType, feature);
-	}
-	
-	private String enumToString(Enum<?> enum1, ConfigurationSection config) {
-		return config.getString(enum1.name().toLowerCase());
-	}
-	
-	private String enumToStringNeg(Enum<?> enum1, ConfigurationSection config) {
-		return config.getString(enum1.name().toLowerCase()+"_neg");
+		FeatureEnumConfig f = sorci.config().enums.feature;
+		switch (featureType) {
+			case ACTION_DEAD:
+				return f.actionDead.value;
+				
+			case ACTION_SPAWN:
+				return f.actionSpawn.value;
+						
+			case APPLY_EXCITED:
+				return f.applyExcited.value;
+				
+			case BOOST_DAMAGE:
+				return value.asNumber() < 0 ? f.boostDamageNeg.value : f.boostDamage.value;
+						
+			case BOOST_HEALTH:
+				return value.asNumber() < 0 ? f.boostHealthNeg.value : f.boostHealth.value;
+						
+			case BOOST_MANA:
+				return value.asNumber() < 0 ? f.boostManaNeg.value : f.boostMana.value;
+						
+			case COPY_CARD_ARENA:
+				return f.copyCardArena.value;
+						
+			case COPY_CARD_ARENA_POSE:
+				return f.copyCardArenaPose.value;
+						
+			case DAMAGE:
+				return f.damage.value;
+						
+			case DESTRUCT:
+				return f.destruct.value;
+						
+			case EXCITED:
+				return f.excited.value;
+						
+			case EXECUTE:
+				return f.execute.value;
+						
+			case GIVE_CARD:
+				return f.giveCard.value;
+						
+			case GIVE_FEATURE_CARD:
+				return f.giveFeatureCard.value;
+						
+			case HEAL:
+				return f.heal.value;
+						
+			case HEALTH:
+				return f.health.value;
+						
+			case HIDE_CARD:
+				return f.hideCard.value;
+						
+			case IF_ATTACKED_EXEC:
+				return f.ifAttackedExec.value;
+						
+			case IF_ATTACKED_EXEC_ONE:
+				return f.ifAttackedExecOne.value;
+						
+			case IF_ATTACKED_GIVE:
+				return f.ifAttackedGive.value;
+						
+			case IF_ATTACKED_GIVE_ONE:
+				return f.ifAttackedGiveOne.value;
+						
+			case IMMOBILIZATION:
+				return f.immobilization.value;
+						
+			case INCITEMENT:
+				return f.incitement.value;
+						
+			case INVOCATION:
+				return f.invocation.value;
+						
+			case INVULNERABILITY:
+				return f.invulnerability.value;
+						
+			case METAMORPH_TO:
+				return f.metamorphTo.value;
+						
+			case REMOVE_CARD:
+				return f.removeCard.value;
+						
+			case REMOVE_MANA_HERO:
+				return value.asNumber() < 0 ? f.removeManaHeroNeg.value : f.removeManaHero.value;
+						
+			case SKIN:
+				return f.skin.value;
+						
+			case STUNNED:
+				return f.stunned.value;
+						
+			case TAKE_NEW_CARD:
+				return f.takeNewCard.value;
+						
+			default:
+				return null;
+		}
 	}
 	
 }
