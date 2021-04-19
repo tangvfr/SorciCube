aa8012bba97f092d5329a309b8907da70b22aac4
BuildTools <unconfigured@null.spigotmc.org>
2021-04-19 13:58:34 +0200

    patch component number

--- SorciCubeApp/src/java/fr/tangv/sorcicubeapp/component/ComponentNumberInt.java
+++ SorciCubeApp/src/java/fr/tangv/sorcicubeapp/component/ComponentNumberInt.java
@@ -7,14 +7,22 @@ public class ComponentNumberInt extends ComponentCustom {
 
 	private static final long serialVersionUID = 5937702865253871122L;
 	private final JSpinner number;
+	private final int min;
+	private final int max;
 		
 	public ComponentNumberInt(String name, int min, int max, int step) {
 		super(name);
+		this.min = min;
+		this.max = max;
 		this.number = new JSpinner(new SpinnerNumberModel(0, min, max, step));
 		this.add(number);
 	}
 	
 	public void setInt(int number) {
+		if (number < min)
+			number = min;
+		else if (number > max)
+			number = max;
 		this.number.setValue(number);
 	}
 	
